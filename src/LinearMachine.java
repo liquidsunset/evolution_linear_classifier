e59482203c5da9403ef978d840f871752ee3ffa8
Daniel Brand <daniel.brand@liquidsunset.org>
2016-10-28 00:26:24 +0200

    initial commit

---
+++ src/LinearMachine.java
@@ -0,0 +1,8 @@
+/**
+ * Created by liquidsunset on 28.10.16.
+ */
+public class LinearMachine {
+    public static void main(String[] args) {
+        System.out.println("Hello World! :)");
+    }
+}
